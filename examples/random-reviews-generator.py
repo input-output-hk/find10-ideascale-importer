fe092668fb923493b5279e48b1d06c02d7e02d6f
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2023-08-10 17:04:04 +0200

    random review generator fix

--- examples/random-reviews-generator.py
+++ examples/random-reviews-generator.py
@@ -15,10 +15,10 @@ fake = Faker()
 assessors_id = sorted(set(fake.unique.random_int(min=5000, max=7000) for i in range(number_of_reviews)))
 with open('random-reviews.csv', mode='w') as file:
   file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-  file_writer.writerow(['id', 'Assessor', 'Impact / Alignment Note', 'Impact / Alignment Rating', 'Feasibility Note', 'Feasibility Rating', 'Auditability Note', 'Auditability Rating', 'level', 'allocated', 'proposal_id', 'proposal_url', 'proposal_title'])
+  file_writer.writerow(['id', 'Assessor', 'Impact / Alignment Note', 'Impact / Alignment Rating', 'Feasibility Note', 'Feasibility Rating', 'Auditability Note', 'Auditability Rating', 'level', 'allocated', 'proposal_id', 'proposal_url', 'proposal_title', 'proposal_challenge_id' ,'Result'])
   index=0
   for proposal in proposals:
     reviews_for_proposal = random.randrange(0, number_of_reviews)
     for n in range(reviews_for_proposal):
-      file_writer.writerow([index+1, assessors_id[n], fake.paragraph(nb_sentences=5), fake.random_int(min=1, max=5),fake.paragraph(nb_sentences=5), fake.random_int(min=1, max=5),fake.paragraph(nb_sentences=5), fake.random_int(min=1, max=5),fake.random_int(min=0, max=1),fake.boolean(chance_of_getting_true=50),proposal['proposal_id'],proposal['proposal_url'], proposal['proposal_title']])
+      file_writer.writerow([index+1, "test_assessor_" + str(assessors_id[n]), fake.paragraph(nb_sentences=7), fake.random_int(min=1, max=5),fake.paragraph(nb_sentences=7), fake.random_int(min=1, max=5),fake.paragraph(nb_sentences=7), fake.random_int(min=1, max=5),fake.random_int(min=0, max=1),fake.boolean(chance_of_getting_true=50),proposal['proposal_id'],proposal['proposal_url'], proposal['proposal_title'],proposal['challenge_id']])
       index+=1
