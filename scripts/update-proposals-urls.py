39b6b6da853c641493724f41223c380acdd36cd3
Joaquín Rosales <joaquin.rosales@iohk.io>
2023-10-23 21:59:23 -0600

    fix: handle unwanted resposes, lint with ruff, format with black

--- scripts/update-proposals-urls.py
+++ scripts/update-proposals-urls.py
@@ -1,21 +1,25 @@
-
 import json
 
-with open('proposals.json') as proposals_file:
+with open("proposals.json") as proposals_file:
     proposals = json.load(proposals_file)
 
-with open('slug-urls.json') as urls_file:
+with open("slug-urls.json") as urls_file:
     urls = json.load(urls_file)
 
 for proposal in proposals:
     found = False
-    for row in urls['data']['fund']['projects']:
-        if row['_id'] == proposal['proposal_id']:
+    for row in urls["data"]["fund"]["projects"]:
+        if row["_id"] == proposal["proposal_id"]:
             found = True
-            proposal['proposal_url'] = 'https://projectcatalyst.io/funds/10/'+row['challenge']['slug']+'/'+row['projectSlug']
+            proposal["proposal_url"] = (
+                "https://projectcatalyst.io/funds/10/"
+                + row["challenge"]["slug"]
+                + "/"
+                + row["projectSlug"]
+            )
 
     if not found:
-        print('Not found for proposal id: ' + proposal['proposal_id'])
+        print("Not found for proposal id: " + proposal["proposal_id"])
 
 json_object = json.dumps(proposals, indent=4)
 with open("proposals-slugs.json", "w") as outfile:
