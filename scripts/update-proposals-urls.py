c22fecd2ba816468db031d428ebf481036e4fc5c
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-11-04 14:46:47 +0100

    feat: update scripts

--- scripts/update-proposals-urls.py
+++ scripts/update-proposals-urls.py
@@ -1,3 +1,11 @@
+# To get the proposals-slugs.json file
+# If you run this graphql query you can access the necessary data to build the URL
+# https://projectcatalyst.io/funds/${fund._id}/${challenge.slug}/${project.projectSlug}
+# https://projectcatalyst.io/api/v1/graphql?explorerURLState=N4IgJg9gxgrgtgUwHYBcQC4QEcYIE4CeABMADpJFEBmMSYAFAPoCWY6RpIAjAAycCUJcpUoAHPBABWCKCgDOQiiMosww5UXFSZKAMoAbGAHN1yqAAsAhvv3IjCRRpFzDJpcoC%2Bpol6W%2BPIB5AA
+# the project._id is the ideascale id
+# The project.slug for the most part is a staight up slugification of the project name, but there are some exceptions worth noting:
+# If there is a name collision or if the resultant name is too long, then the slug is shortened and a unique short hash is added
+
 import json
 
 with open("proposals.json") as proposals_file:
